8686051260041d6ac3a6f7821edf23b1bb5daee0
Ge Jin <jinwar@gmail.com>
2013-10-31 15:25:00 -0400

    delete OBS_recf.m

--- OBS_recf.m
+++
@@ -1,8 +0,0 @@
-%% scripts to test the method to clean up OBS receiver function by calculate the transform function between
-%  tangental comp and radial comp
-%  written by Ge Jin, jinwar@gmail.com
-%  2013.10.25
-
-clear;
-
-
